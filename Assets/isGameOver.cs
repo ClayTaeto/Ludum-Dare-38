4312e1f4f4e4da715226f8384ad8cb0bb6ce0487
Sdonai <silentstrum@gmail.com>
2017-04-23 03:01:42 -0700

    game over screen

--- Assets/isGameOver.cs
+++ Assets/isGameOver.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class isGameOver : MonoBehaviour {
-
+	private GameObject[] enemies;
+	public float timeSinceCheck = 0;
+	private float timeBetweenChecks = .600f;
+	public GameObject[] ui;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,18 @@ public class isGameOver : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		timeSinceCheck += Time.deltaTime;
+		if (timeSinceCheck > timeBetweenChecks) {
+			timeSinceCheck = 0;
+			enemies = GameObject.FindGameObjectsWithTag("Enemy");
+			Debug.Log("Checking for Enemies");
+			if (enemies.Length == 0) {
+				Debug.Log ("No enemies");
+				foreach (GameObject bit in ui) {
+					bit.SetActive (true);
+				}
+
+			}
+		}
 	}
 }
